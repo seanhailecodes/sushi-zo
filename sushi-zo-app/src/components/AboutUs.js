f6d0f035791bb05e6c3ac348770e89451416760f
icelydone <soren03@hotmail.com>
2019-04-18 14:50:53 -0400

    Finished About-Us component

--- sushi-zo-app/src/components/AboutUs.js
+++ sushi-zo-app/src/components/AboutUs.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './AboutUs.css'
 
 const AboutUs = () => {
   return (
     <div className="about-us">
-      <img src="" />
-      <div>
+      <img src={require("../images/About Us.png")} alt="About text" />
+      <div className="about-us-text">
         <h2>ABOUT US</h2>
         <p>Sushi Zo is created "Ittai-ken"-style. Ittai-jan is when the neta and shari unite in perfect harmony. The balance between the neta (the seafood) and shari (sushi rice) is the most important aspect of sushi.</p>
         <p>Our rice is meticulously cooked, seasoned with a unique blend of vinegars and served at body temperature, to create the perfect partner to the "neta".</p>
--- sushi-zo-app/src/components/LocationNav.js
+++ sushi-zo-app/src/components/LocationNav.js
@@ -3,7 +3,11 @@ import React from 'react'
 const LocationNav = () => {
   return (
     <nav>
-      <h2>Location nav</h2>
+      <li><h3>New York</h3></li>
+      <li><h3>Los Angeles</h3></li>
+      <li><h3>Tokyo</h3></li>
+      <li><h3>Bangkok</h3></li>
+      <li><h3>Hong Kong</h3></li>
     </nav>
   )
 }
