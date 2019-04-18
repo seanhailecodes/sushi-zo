eee179192ca1e6594f13e0d53520a728564b9909
icelydone <soren03@hotmail.com>
2019-04-18 13:50:23 -0400

    Added AboutUs content

--- sushi-zo-app/src/components/AboutUs.js
+++ sushi-zo-app/src/components/AboutUs.js
@@ -3,7 +3,13 @@ import React from 'react'
 const AboutUs = () => {
   return (
     <div className="about-us">
-      <h1>About us</h1>
+      <img src="" />
+      <div>
+        <h2>ABOUT US</h2>
+        <p>Sushi Zo is created "Ittai-ken"-style. Ittai-jan is when the neta and shari unite in perfect harmony. The balance between the neta (the seafood) and shari (sushi rice) is the most important aspect of sushi.</p>
+        <p>Our rice is meticulously cooked, seasoned with a unique blend of vinegars and served at body temperature, to create the perfect partner to the "neta".</p>
+        <p>With many of our sushi selections, we use perfectly matched accompaniment for the particular "neta", which may include our unique soy sauce, sea, salt and other ingredients.</p>
+      </div>
     </div>
   )
 }
