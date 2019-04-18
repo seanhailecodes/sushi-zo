6bc58ff05c8d0c51c5d6fb022fc3283526139704
Charles Cui <xarlze@ool-ad0205b7.dyn.optonline.net>
2019-04-18 14:39:01 -0400

    landing display

--- sushi-zo-app/src/components/Nav.js
+++ sushi-zo-app/src/components/Nav.js
@@ -1,16 +1,28 @@
 import React from 'react'
+import './Nav.css'
+const opaqueLogo = require('../images/opaquelogo.png')
 
-const Nav = () => {
+const Nav = props => {
   return (
-    <nav>
+    <nav
+      id="mainNav"
+      style={(props.scrollY>props.sizeY-80)?{
+        top:"0",
+        marginTop:"0",
+        position:"fixed"
+      }:null}
+    >
       <ul>
+        <img
+          id="opaquelogo"
+          src={opaqueLogo}
+        />
         <li><a href="">omakase</a></li>
         <li><a href="">about us</a></li>
         <li><a href="">contact</a></li>
-        <li><a href=""><img src=""/></a></li>
         <li><a href="">locations</a></li>
         <li><a href="">press</a></li>
-        <li><button><a href="">book</a></button></li>
+        {/* <li><button><a href="">book</a></button></li> */}
       </ul>
     </nav>
   )
